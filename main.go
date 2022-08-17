bebaa5b4432dbab5b70d2e42a3a46777b0bb5668
yeyu <zhenpsm@163.com>
2022-08-17 17:49:06 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,8 +6,6 @@ import (
 	"github.com/go-rod/stealth"
 	"github.com/gogf/gf/os/gfile"
 	"github.com/gogf/gf/os/gres"
-	"github.com/gogf/gf/util/gconv"
-	"math/rand"
 	"time"
 )
 
@@ -25,35 +23,16 @@ func main() {
 	iframe := page.MustElement("#tcaptcha_iframe").MustWaitVisible().MustFrame()
 	iframe.MustElement("#slider").MustWaitVisible()
 
-	pt := iframe.MustElement("#slider").MustShape().OnePointInside()
+	pt := iframe.MustElement("#slider").MustShape().Box()
 
-	x := page.MustEval(JsGetElementViewX(), "tcaptcha_transform").Str()
-	y := page.MustEval(JsGetElementViewY(), "tcaptcha_transform").Str()
-	ix := iframe.MustEval(JsGetElementViewX(), "sliderbg").Str()
-	iy := iframe.MustEval(JsGetElementViewY(), "sliderbg").Str()
-	startWidth := pt.X - 20
-	startHeight := pt.Y - 20
-	endWidth := 280
-	endHeight := 40
-	iframe.Overlay(startWidth, startHeight, float64(endWidth), float64(endHeight), "iframe")
-	rand.Seed(time.Now().Unix())
-	startX := gconv.Float64(x) + gconv.Float64(ix) + gconv.Float64(rand.Intn(30)+10)
-	startY := gconv.Float64(y) + gconv.Float64(iy) + gconv.Float64(rand.Intn(30)+10)
-	iframe.Mouse.MustMove(startX, startY)
+	iframe.Overlay(pt.X, pt.Y, float64(280), pt.Height, "iframe")
+	iframe.Mouse.MustMove(pt.X+5, pt.Y+8)
 	iframe.Mouse.MustClick("left")
 	time.Sleep(3 * time.Second)
-	iframe.Mouse.MustMove(startX+50, startY)
-	iframe.Mouse.MustUp("left")
+	iframe.Mouse.MustMove(pt.X+50, pt.Y)
+	// iframe.Mouse.MustUp("left")
 
 	time.Sleep(30 * time.Second)
-}
-
-func JsGetElementViewX() string {
-	return `(path)=>document.getElementById(path).getBoundingClientRect().left + document.documentElement.scrollLeft`
-}
-
-func JsGetElementViewY() string {
-	return `(path)=>document.getElementById(path).getBoundingClientRect().top + document.documentElement.scrollTop`
 }
 
 func GetCurrentAbPathByCaller() string {
